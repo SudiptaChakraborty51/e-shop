2f0ffb374837832614a2d00e656a11500457ba5f
Sudipta <hisudipta26@gmail.com>
2022-07-15 14:42:18 +0530

    modified

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,18 +1,21 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons';
 import React from 'react'
+import { useState } from 'react';
 import styled from 'styled-components'
+import {sliderItems} from '../data'
 
 const Container =styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
-    position: relative
+    position: relative;
+    overflow: hidden
 `;
 
 const Arrow = styled.div`
     width: 50px;
     height: 50px;
-    background-color: #fff7f7;
+    background-color: #ccfbf1;
     border-radius: 50%;
     display: flex;
     align-items: center;
@@ -25,10 +28,13 @@ const Arrow = styled.div`
     margin: auto;
     cursor: pointer;
     opacity: 0.5;
+    z-index: 2
 `;
 
 const Wrapper = styled.div`
     height: 100%;
+    display: flex;
+    transform: translateX(0vw)
 `;
 
 const Slide = styled.div`
@@ -44,51 +50,58 @@ const Imgcontainer = styled.div`
 `;
 
 const Image = styled.img`
-    height: 80%;
+    height: 100%;
+    padding: 60px;
 `;
 
 const InfoContainer = styled.div`
     flex: 1;  
-    padding: 50px;  
+    padding: 10px;  
 `;
 
 const Title = styled.h1`
-    font-size: 70px;
+    font-size: 50px;
 `;
 
 const Desc = styled.p`
     margin: 50px 0px;
     font-size: 20px;
     font-weight: 500;
-    letter-spacing; 3px;
+    letter-spacing: 3px;
 `;
 
 const Button = styled.button`
     padding: 10px;
     font-size: 20px;
-    background-color: transparent;
+    background-color: #ccfbf1;
     cursor: pointer;
 `;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const clickHandler = (direction)=> {
+
+  }
   return (
     <Container>
-        <Arrow direction="left">
+        <Arrow direction="left" onClick={()=>clickHandler("left")}>
             <ArrowLeftOutlined />
         </Arrow>
         <Wrapper>
-            <Slide>
-                <Imgcontainer>
-                    <Image src="e-shop\src\pictures\S1.jpeg"></Image>
-                </Imgcontainer>
-                <InfoContainer>
-                    <Title>SUMMER SALE</Title>
-                    <Desc>DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.</Desc>
-                    <Button>SHOW NOW</Button>
-                </InfoContainer>
-            </Slide>
+            {sliderItems.map((item)=>(
+                <Slide bg={item.bg}>
+                    <Imgcontainer>
+                        <Image src={item.img}></Image>
+                    </Imgcontainer>
+                    <InfoContainer>
+                        <Title>{item.title}</Title>
+                        <Desc>{item.desc}</Desc>
+                        <Button>SHOP NOW</Button>
+                    </InfoContainer>
+                </Slide>
+            ))}
         </Wrapper>
-        <Arrow direction="right">
+        <Arrow direction="right" onClick={()=>clickHandler("right")}>
             <ArrowRightOutlined />
         </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,27 @@
+import S1 from '../pictures/S1.webp'
+import S2 from '../pictures/S2.webp'
+import S3 from '../pictures/S3.webp'
+
+export const sliderItems = [
+    {
+      id: 1,
+      img: {S1},
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: {S2},
+      title: "TOP BRAND COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: {S3},
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
